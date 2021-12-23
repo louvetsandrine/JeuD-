ceb83ec407c45ab4075c48b93f36d0130d601338
Sandrine LOUVET <louvetsandrine@hotmail.fr>
2021-12-23 15:52:58 +0100

    23/12/2021 first

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonStart = document.getElementById("buttonStart");
 const startPlayerOne = document.getElementById("startPlayerOne");
+const startPlayerTwo = document.getElementById("startPlayerTwo");
 const buttonRollDice = document.getElementById("buttonRollDice");
 const buttonHold = document.getElementById("buttonHold");
 const diceSpace = document.getElementById("diceSpace");
@@ -17,32 +18,34 @@ const walletPlayerTwo = document.getElementById("walletPlayerTwo");
 
 const dice = [ 
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi dice bi-dice-1 text-danger" viewBox="0 0 16 16" id="diceOne"><circle cx="8" cy="8" r="1.5"/><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/></svg>', 
+      picture:'<img class="img-fluid" src="images/dice-1.png" id="diceOne"/>', 
       score:1
    },
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-dice-2 text-danger" viewBox="0 0 16 16" id="diceTwo"><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/></svg>',
+      picture:'<img class="img-fluid" src="images/dice-2.png" id="diceTwo"/>',
       score:2
    },
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-dice-3 text-danger" viewBox="0 0 16 16" id="diceThree"><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/></svg>',
+      picture:'<img class="img-fluid" src="images/dice-3.png" 0" id="diceThree"/>',
       score:3
    },
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-dice-4 text-danger" viewBox="0 0 16 16" id="diceFour"><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/></svg>',
+      picture:'<img class="img-fluid" src="images/dice-4.png" id="diceFour"/>',
       score:4
    },
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-dice-5 text-danger" viewBox="0 0 16 16" id="diceFive"><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/></svg>',
+      picture:'<img class="img-fluid" src="images/dice-5.png" id="diceFive"/>',
       score:5
    },
    {
-      picture:'<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-dice-6 text-danger" viewBox="0 0 16 16" id="diceSix"><path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/></svg>',
+      picture:'<img class="img-fluid" src="images/dice-6.png" id="diceSix"/>',
       score:6
    }
 ]
 
-let scoreStart = 0;
+let scoreStartOne = 0;
+let scoreStartTwo = 0;
+let player = "one";
 
 
 
@@ -53,16 +56,27 @@ buttonStart.addEventListener("click", ()=>{
    buttonRollDice.className += "active";
    buttonHold.className += "active";
    startPlayerOne.style.color = "red";
+
 })
 
 buttonRollDice.addEventListener("click", ()=>{
+
+   player = player == "one"? "two" : "one";
    
-   const randomNumber = Math.floor(Math.random()*dice.length);
+   let randomNumber = Math.floor(Math.random()*dice.length);
    console.log(randomNumber);
 
    diceSpace.innerHTML = dice[randomNumber]["picture"] + '<p id="diceScore">Résultat: ' + dice[randomNumber]["score"] + '</p>';
-   scoreStart += dice[randomNumber]["score"];
-   diceScorePlayerOne.innerHTML = scoreStart;
+   scoreStartOne += dice[randomNumber]["score"];
+   diceScorePlayerOne.innerHTML = scoreStartOne;
+
+   if(dice[randomNumber]["score"] === 1){
+      startPlayerOne.style.color = "black";
+      startPlayerTwo.style.color = "red";
+      diceScorePlayerOne.innerHTML = 0;
+      scoreStartTwo += dice[randomNumber]["score"];
+      diceScorePlayerTwo.innerHTML = scoreStartTwo;
+   }
 
 
 })
@@ -71,7 +85,7 @@ buttonRollDice.addEventListener("click", ()=>{
 
 buttonHold.addEventListener("click", ()=>{
    
-   console.log("tt", scoreStart)
-   walletPlayerOne.innerHTML = scoreStart;
+   console.log("tt", scoreStartOne)
+   walletPlayerOne.innerHTML = scoreStartOne;
 
 })
